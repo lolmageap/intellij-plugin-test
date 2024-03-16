578376b8b60c839a24cab159444dc7f87c6c5da4
lolmageap <ekxk1234@naver.com>
2024-03-16 22:15:52 +0900

    create message in menu

---
+++ src/main/kotlin/com/example/test/Menu.kt
@@ -0,0 +1,12 @@
+package com.example.test
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.ui.Messages
+
+class Menu: AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        val menu = Messages.showInputDialog("What is your name?", "Input your name", Messages.getQuestionIcon())
+        Messages.showMessageDialog(menu, "input", null)
+    }
+}
