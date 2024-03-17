eb36f61b326427b36855d1335bfd12af374d0faf
lolmageap <ekxk1234@naver.com>
2024-03-17 22:51:41 +0900

    create byte code action

---
+++ src/main/kotlin/com/example/test/bytecode/ByteCode.kt
@@ -0,0 +1,9 @@
+package com.example.test.bytecode
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class ByteCode: AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+    }
+}
