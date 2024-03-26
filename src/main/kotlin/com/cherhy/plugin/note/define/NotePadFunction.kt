f083913b184191c88322daef1fb617b38fa1599a
lolmageap <ekxk1234@naver.com>
2024-03-26 23:41:08 +0900

    create default file

--- src/main/kotlin/com/cherhy/plugin/note/define/NotePadFunction.kt
+++ src/main/kotlin/com/cherhy/plugin/note/define/NotePadFunction.kt
@@ -2,10 +2,13 @@ package com.cherhy.plugin.note.define
 
 import com.cherhy.plugin.function.rootPackage
 import com.cherhy.plugin.note.ui.Notepad
+import java.io.File
 import javax.swing.JFileChooser
 
 fun Notepad.new() {
     textArea.isVisible = true
+    this.isVisible = true
+    createDefaultFile()
 }
 
 fun Notepad.open() {
@@ -22,7 +25,20 @@ fun Notepad.open() {
     }
 }
 
+fun Notepad.createDefaultFile() {
+    val defaultDirectory = rootPackage(project)
+    val fileToSave = File(defaultDirectory, currentFileName)
+    fileToSave.writeText(textArea.text)
+}
+
+// TODO : 중복 제거 하기
 fun Notepad.saveTextToFile() {
+    val defaultDirectory = rootPackage(project)
+    val fileToSave = File(defaultDirectory, currentFileName)
+    fileToSave.writeText(textArea.text)
+}
+
+fun Notepad.saveAndClose() {
     val defaultDirectory = rootPackage(project)
 
     JFileChooser().apply {
