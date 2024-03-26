6181c5a9e00e86c821023834dace517deb3be7ef
lolmageap <ekxk1234@naver.com>
2024-03-26 23:40:38 +0900

    fix to auto save

--- src/main/kotlin/com/cherhy/plugin/note/ui/Notepad.kt
+++ src/main/kotlin/com/cherhy/plugin/note/ui/Notepad.kt
@@ -1,20 +1,26 @@
 package com.cherhy.plugin.note.ui
 
+import com.cherhy.plugin.function.rootPackage
 import com.cherhy.plugin.note.constant.NoteConstant.CLOSE
-import com.cherhy.plugin.note.constant.NoteConstant.SAVE
-import com.intellij.openapi.project.Project
-import java.awt.BorderLayout.*
-import javax.swing.*
 import com.cherhy.plugin.note.define.saveTextToFile
+import com.intellij.openapi.project.Project
+import java.awt.BorderLayout.CENTER
 import java.awt.Dimension
-import java.awt.Toolkit
 import java.awt.event.ActionEvent
 import java.awt.event.KeyEvent
+import java.io.File
+import java.util.UUID
+import javax.swing.*
+import javax.swing.event.DocumentEvent
+import javax.swing.event.DocumentListener
 
 class Notepad(
     val textArea: JTextArea,
     val project: Project,
 ): JFrame() {
+
+    val currentFileName = UUID.randomUUID().toString() + ".txt"
+
     init {
         title = "Notepad"
         defaultCloseOperation = DISPOSE_ON_CLOSE
@@ -26,31 +32,30 @@ class Notepad(
         val scrollPane = JScrollPane(textArea)
         add(scrollPane, CENTER)
 
-        val saveButton = JButton(SAVE)
-        add(saveButton, SOUTH)
-        saveButton.addActionListener { saveTextToFile() }
-
-        saveEvent()
-        closeEvent()
-    }
-
-    private fun saveEvent() {
-        val saveKeyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_S, Toolkit.getDefaultToolkit().menuShortcutKeyMask, false)
-        val saveAction = object : AbstractAction() {
-            override fun actionPerformed(e: ActionEvent?) {
+        textArea.document.addDocumentListener(object : DocumentListener {
+            override fun insertUpdate(e: DocumentEvent?) {
                 saveTextToFile()
             }
-        }
 
-        rootPane.inputMap.put(saveKeyStroke, SAVE)
-        rootPane.actionMap.put(SAVE, saveAction)
+            override fun removeUpdate(e: DocumentEvent?) {
+                saveTextToFile()
+            }
+
+            override fun changedUpdate(e: DocumentEvent?) {
+                saveTextToFile()
+            }
+        })
+
+        closeEvent()
     }
 
     private fun closeEvent() {
         val escKeyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0, false)
         val escAction = object : AbstractAction() {
             override fun actionPerformed(e: ActionEvent?) {
-                dispose()
+                if (confirmAndSaveFile()) {
+                    dispose()
+                }
             }
         }
 
@@ -58,6 +63,34 @@ class Notepad(
         rootPane.actionMap.put(CLOSE, escAction)
     }
 
-    // TODO: ctrl + s to save
+    private fun confirmAndSaveFile(): Boolean {
+        val defaultDirectory = rootPackage(project)
+        val fileChooser = JFileChooser(defaultDirectory)
+        fileChooser.dialogTitle = "Save"
+        fileChooser.fileSelectionMode = JFileChooser.FILES_ONLY
+
+        val userSelection = fileChooser.showSaveDialog(this)
+
+        if (userSelection == JFileChooser.APPROVE_OPTION) {
+            val newFileName = JOptionPane.showInputDialog(
+                this,
+                "Enter new file name:",
+                "File Name",
+                JOptionPane.PLAIN_MESSAGE
+            )
+
+            if (newFileName.isNullOrEmpty()) {
+                return false
+            }
+
+            val newFilePath = File(defaultDirectory, "$newFileName.txt")
+
+            newFilePath.writeText(textArea.text)
+            return true
+        }
+        return false
+    }
+
+    // TODO: 항상 자동 저장이 되게 수정
     // TODO: 다른 화면을 누르면 닫히는 현상 수정
 }
